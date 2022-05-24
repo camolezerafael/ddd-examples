9a0a9911e9a6f330b3d7e7915ae7ea2450be80e7
Rafael Camoleze <contato@rafaelcamoleze.com>
2022-05-24 19:34:30 -0300

    first commit

---
+++ src/entity/customer.ts
@@ -0,0 +1,59 @@
+import Address from './address'
+
+export default class Customer {
+
+	private _id: string
+	private _name: string
+	private _address!: Address
+	private _active: boolean = false
+
+	constructor(id: string, name: string) {
+		this._id = id
+		this._name = name
+		this.validate()
+	}
+
+	get name(): string {
+		return this._name
+	}
+
+	validate(): boolean {
+		if(this._id.length === 0) {
+			throw new Error('Id is required')
+		}
+		if(this._name.length === 1) {
+			throw new Error('Name is required')
+		}
+		return true;
+	}
+
+	changeName(name: string) {
+		this._name = name
+		this.validate()
+	}
+
+	isActive(): boolean {
+		return this._active
+	}
+
+	activate() {
+		if(this._address === undefined) {
+			throw new Error('Address is mandatory to activate a customer')
+		}
+		this._active = true
+	}
+
+	deactivate() {
+		this._active = false
+	}
+
+	set Address(address: Address) {
+		this._address = address
+	}
+}
+
+const costumer = new Customer('123', '')
+
+
+
+
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import Address from './entity/address'
+import Customer from './entity/customer'
+import OrderItem from './entity/order_item'
+import Order from './entity/order'
+
+let costumer = new Customer('123', 'Rafael Camoleze')
+const address = new Address('Rua treze', 2, '21312-123', 'São Paulo')
+
+costumer.Address = address
+costumer.activate()
+
+const item1 = new OrderItem('1', 'Item 1', 10, '123', 1)
+const item2 = new OrderItem('2', 'Item 2', 15, '123', 1)
+
+const order = new Order('1', '123', [item1, item2])
